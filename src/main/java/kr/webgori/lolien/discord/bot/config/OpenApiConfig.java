d79b44e651a7adfa0138b213cf2af5e1b725caa8
webgori <webgori@gmail.com>
2020-02-09 23:39:32 +0900

    Swagger 설정 Class명 변경

--- src/main/java/kr/webgori/lolien/discord/bot/config/OpenApiConfig.java
+++ src/main/java/kr/webgori/lolien/discord/bot/config/OpenApiConfig.java
@@ -18,7 +18,7 @@ import org.springframework.context.annotation.Configuration;
     )
 )
 @Configuration
-public class SwaggerConfig {
+public class OpenApiConfig {
   /**
    * customGameOpenApi.
    * @return GroupedOpenApi
