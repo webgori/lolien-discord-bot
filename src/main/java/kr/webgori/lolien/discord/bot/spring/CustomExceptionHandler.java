2593bbbca6c9f426fc456c323e532f33dd7f0832
webgori <webgori@gmail.com>
2020-08-28 00:19:56 +0900

    예외 처리 수정

--- src/main/java/kr/webgori/lolien/discord/bot/spring/CustomExceptionHandler.java
+++ src/main/java/kr/webgori/lolien/discord/bot/spring/CustomExceptionHandler.java
@@ -1,7 +1,11 @@
 package kr.webgori.lolien.discord.bot.spring;
 
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import javax.servlet.http.HttpServletRequest;
 import kr.webgori.lolien.discord.bot.exception.AlreadyAddedSummonerException;
 import kr.webgori.lolien.discord.bot.exception.SummonerNotFoundException;
+import kr.webgori.lolien.discord.bot.response.ExceptionResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -15,6 +19,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @RequiredArgsConstructor
 @ControllerAdvice
 public class CustomExceptionHandler {
+  private final HttpServletRequest httpServletRequest;
+
   @ExceptionHandler(value = BadCredentialsException.class)
   public ResponseEntity<Void> badCredentialsException() {
     HttpStatus httpStatus = HttpStatus.UNAUTHORIZED;
@@ -34,9 +40,28 @@ public class CustomExceptionHandler {
   }
 
   @ExceptionHandler(value = IllegalArgumentException.class)
-  public ResponseEntity<Void> illegalArgumentException() {
+  public ResponseEntity<ExceptionResponse> illegalArgumentException(
+      IllegalArgumentException exception) {
+
+    String message = exception.getMessage();
+
     HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
-    return new ResponseEntity<>(httpStatus);
+    int status = httpStatus.value();
+    String error = httpStatus.getReasonPhrase();
+
+    String requestUri = httpServletRequest.getRequestURI();
+
+    LocalDateTime dateTime = LocalDateTime.now();
+    long timestamp = Timestamp.valueOf(dateTime).getTime();
+
+    ExceptionResponse exceptionResponse = new ExceptionResponse();
+    exceptionResponse.setTimestamp(timestamp);
+    exceptionResponse.setPath(requestUri);
+    exceptionResponse.setMessage(message);
+    exceptionResponse.setStatus(status);
+    exceptionResponse.setError(error);
+
+    return new ResponseEntity<>(exceptionResponse, httpStatus);
   }
 
   @ExceptionHandler(value = AccessDeniedException.class)
