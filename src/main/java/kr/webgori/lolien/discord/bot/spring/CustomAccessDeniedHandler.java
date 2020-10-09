1b9abee25a5fed9c3d85ff3236f4be87a2e72d08
webgori <webgori@gmail.com>
2020-10-09 17:58:17 +0900

    findbugs 수정

--- src/main/java/kr/webgori/lolien/discord/bot/spring/CustomAccessDeniedHandler.java
+++ src/main/java/kr/webgori/lolien/discord/bot/spring/CustomAccessDeniedHandler.java
@@ -2,7 +2,6 @@ package kr.webgori.lolien.discord.bot.spring;
 
 import java.io.IOException;
 import java.util.Objects;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -10,6 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.web.servlet.HandlerExecutionChain;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 @RequiredArgsConstructor
@@ -21,7 +21,7 @@ public class CustomAccessDeniedHandler implements AccessDeniedHandler {
   public void handle(
       HttpServletRequest httpServletRequest,
       HttpServletResponse httpServletResponse,
-      AccessDeniedException accessDeniedException) throws IOException, ServletException {
+      AccessDeniedException accessDeniedException) throws IOException {
 
     logger.error("", accessDeniedException);
 
@@ -29,9 +29,12 @@ public class CustomAccessDeniedHandler implements AccessDeniedHandler {
     String message = "아이디 또는 비밀번호를 확인해주세요.";
 
     try {
-      Objects
-          .requireNonNull(requestMappingHandlerMapping.getHandler(httpServletRequest))
-          .getHandler();
+      HandlerExecutionChain handler = requestMappingHandlerMapping.getHandler(httpServletRequest);
+
+      if (Objects.isNull(handler)) {
+        httpStatus = HttpStatus.NOT_FOUND;
+        message = HttpStatus.NOT_FOUND.getReasonPhrase();
+      }
     } catch (Exception e) {
       logger.info("", e);
       httpStatus = HttpStatus.NOT_FOUND;
