6ab3867d76b151356f4e26ac83f1092e85add6ae
박용순 <webgori@gmail.com>
2020-12-30 22:08:49 +0900

    debug용 log 추가

--- src/main/java/kr/webgori/lolien/discord/bot/component/AuthenticationComponent.java
+++ src/main/java/kr/webgori/lolien/discord/bot/component/AuthenticationComponent.java
@@ -63,6 +63,8 @@ public class AuthenticationComponent {
     String email = user.getEmail();
     String hash = getHash(now, email);
 
+    logger.error("now: {}, hash: {}", now, hash);
+
     return getUserSessionDto(now, user, hash);
   }
 
